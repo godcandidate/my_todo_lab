bc920379e48a38187d140d9165ee3d84b446ef4b
godcandidate <godcandidate101@gmail.com>
2025-08-14 08:38:32 +0000

    chore(app): refactor CORS configuration and clean up code formatting

--- todo_backend/app.ts
+++ todo_backend/app.ts
@@ -1,38 +1,34 @@
-import express,  { Request, Response} from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import 'dotenv/config';
-import todoRouter from './routes/route';
-
-
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import todoRouter from "./routes/route";
 
 const app = express();
 
-
 //body parser
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 //cookie parser
 app.use(cookieParser());
 
 //cors
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
-
-// routes
 app.use(
-  "/api/v1",
-  todoRouter,
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
 );
 
-//testing api
-app.use('/test', (req: Request, res:Response) => {
+// routes
+app.use("/api/v1", todoRouter);
+
+//health check
+app.use("/", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
-    message: "API is working"
+    message: "API is working",
   });
-}); 
+});
 
-
-export {app};
+export { app };
