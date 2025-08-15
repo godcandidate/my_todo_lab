674af3a895fe38379a894a42fd4e9aff66cae327
godcandidate <godcandidate101@gmail.com>
2025-08-15 08:57:55 +0000

    chore: add winston and its types for improved logging capabilities

--- todo_backend/app.ts
+++ todo_backend/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import todoRouter from "./routes/route";
+import winston from "winston";
 
 const app = express();
 
@@ -19,6 +20,27 @@ app.use(
     credentials: true,
   })
 );
+
+// Simple logger setup for loki
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.Console()],
+});
+
+// Log requests
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    logger.info({
+      method: req.method,
+      url: req.originalUrl,
+      status: res.statusCode,
+      responseTime: `${Date.now() - start}ms`,
+    });
+  });
+  next();
+});
 
 // routes
 app.use("/api/v1", todoRouter);
