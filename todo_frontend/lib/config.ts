501c0240ccc6c1daa111b1b1f3a6f93cce164194
godcandidate <godcandidate101@gmail.com>
2025-10-30 15:06:25 +0000

    chore(amplify): configure for amplify setup

--- todo_frontend/lib/config.ts
+++ todo_frontend/lib/config.ts
@@ -10,5 +10,5 @@ export const getApiBaseUrl = () => {
   if (typeof window !== 'undefined' && window.__ENV__?.API_BASE_URL) {
     return window.__ENV__.API_BASE_URL;
   }
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000/api/v1/todos';
+  return process.env.NEXT_PUBLIC_API_BASE_URL || '/api/v1/todos';
 };
